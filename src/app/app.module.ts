e62dcc40e711c992c128714484b26ccb70248f50
alefari <ale.jf09@gmail.com>
2018-10-07 16:00:58 -0400

    Creada pagina de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BarraComponent } from './components/barra/barra.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule
