c4b677a342b564b672ea95a34889ebf1285980c5
mrodp <m.rodriguez2016@correo.unimet.edu.ve>
2018-10-08 16:48:42 -0400

    commit obligado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BarraComponent } from './components/barra/barra.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraComponent,
+    GridComponent,
   ],
   imports: [
     BrowserModule
