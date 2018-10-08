83221739f05bfe786f4d03d5ecb838f7e81187b8
alefari <ale.jf09@gmail.com>
2018-10-08 16:50:46 -0400

    Agregado Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BarraComponent } from './components/barra/barra.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarraComponent,
     LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule
