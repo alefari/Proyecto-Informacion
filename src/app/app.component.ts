6c4d1791f0a0ed6a873fd915ce93aa346f9d3eb7
alefari <ale.jf09@gmail.com>
2018-10-09 11:11:02 -0400

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+
+import { from } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BarraComponent } from './components/barra/barra.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const router: Routes = [
+  {
+    path: 'Dashboard',
+    component: DashboardComponent
+  },
+
+  {
+    path: 'Login',
+    component: LoginComponent
+  }
+
+];
 
 @NgModule({
   declarations: [
@@ -14,9 +28,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     DashboardComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
