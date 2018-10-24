bdaed4161765f0186ba90d453f73d0a9317376a2
alefari <ale.jf09@gmail.com>
2018-10-24 14:40:56 -0400

    commit para male

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BarraComponent } from './components/barra/barra.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TarjetaComponent } from './components/tarjeta/tarjeta.component';
 
 const router: Routes = [
   {
@@ -16,7 +17,12 @@ const router: Routes = [
   {
     path: 'Login',
     component: LoginComponent
-  }
+  },
+
+  {
+    path: '',
+    component: LoginComponent
+  },
 
 ];
 
@@ -26,6 +32,7 @@ const router: Routes = [
     BarraComponent,
     LoginComponent,
     DashboardComponent,
+    TarjetaComponent,
   ],
   imports: [
     BrowserModule,
