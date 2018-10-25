6e2f5809488e415cb31bcb866a4cb9a93452864a
alefari <ale.jf09@gmail.com>
2018-10-25 07:28:01 -0400

    commit para compartir

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { of as observableOf } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  uid = observableOf('123');
+  isAdmin = observableOf(true);
+  constructor() { }
+}
